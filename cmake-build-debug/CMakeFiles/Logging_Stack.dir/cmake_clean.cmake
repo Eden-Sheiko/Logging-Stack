2a0467a77b0ccdc43b9741f46a8d540e10ca2726
Eden <edensheiko@gmail.com>
2022-10-26 12:13:34 +0300

    init proj

---
+++ cmake-build-debug/CMakeFiles/Logging_Stack.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Logging_Stack.dir/main.cpp.o"
+  "CMakeFiles/Logging_Stack.dir/main.cpp.o.d"
+  "Logging_Stack"
+  "Logging_Stack.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Logging_Stack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
